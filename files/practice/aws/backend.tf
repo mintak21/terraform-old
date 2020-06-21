18cee84b16a95250f609e44b5dcea8afcfe508d9
mintak-Macbook <mintak21@gmail.com>
2020-06-21 16:49:13 +0900

    [feat] use backend S3 :+1:

--- files/practice/aws/backend.tf
+++ files/practice/aws/backend.tf
@@ -1,10 +1,20 @@
 terraform {
-  backend remote {
+  # use AWS S3 Bucket
+  backend s3 {
+    bucket         = "mintak-tfstate-manage"
+    key            = "mintak-terraformer/terraform.tfstate"
+    dynamodb_table = "mintak-terraform-tfstate-lock"
+    region         = "ap-northeast-1"
+    encrypt        = true
+  }
+
+  # use Terraform Cloud
+  /* backend remote {
     hostname     = "app.terraform.io"
     organization = "mintak21"
 
     workspaces {
       name = "terraform"
     }
-  }
+  } */
 }
--- files/practice/aws/providers.tf
+++ files/practice/aws/providers.tf
@@ -1,5 +1,5 @@
 provider aws {
-  version = ">=2.67.0"
+  version = "~> 2.67"
   region  = "ap-northeast-1" # required
   profile = "mintak-terraform"
 }
