351bf7854c90758dee20be04dc1b9da124576d2d
mintak-Macbook <mintak21@gmail.com>
2020-06-27 02:26:18 +0900

    [feat] add codebuild settings :sparkles:

---
+++ cicd/variables.tf
@@ -0,0 +1,16 @@
+variable project_name {
+  type        = string
+  description = "CI/CDプロジェクト名称"
+  default     = "mintak-terraform-ci-cd"
+}
+
+variable buildspec_settings {
+  type        = string
+  description = "ビルド仕様"
+  default     = "buildspec.yml"
+}
+
+variable github_repogitory_location {
+  type        = string
+  description = "連携するGithubリポジトリURL"
+}
