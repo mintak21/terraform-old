5081b3a5fa9553b8563f294a55a874602ba1fc6c
mintak-Macbook <mintak21@gmail.com>
2020-06-27 04:38:21 +0900

    [feat] fix install :bug:

--- scripts/ci/install_packages.sh
+++ scripts/ci/install_packages.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 function install_tflint() {
-  curl -L "$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest grep -o -E "https://.+?_linux_amd64.zip")" \
-       -o tflint.zip && unzip tflint.zip && rm tflint.zip
-  printf '\033[36m%s\033[m\n' "install tflint."
+  REPO_URL="https://github.com/terraform-linters/tflint/releases/download"
+  DL_LINK="${REPO_URL}/v0.16.2/tflint_linux_amd64.zip"
+  wget ${DL_LINK} -P /tmp
+  unzip /tmp/tflint_linux_amd64.zip -d /tmp
+  mv /tmp/tflint /bin/tflint
+  echo "install tflint"
 }
 
 function install_tfnotify() {
   REPO_URL="https://github.com/mercari/tfnotify/releases/download"
   DL_LINK="${REPO_URL}/v0.6.2/tfnotify_linux_amd64.tar.gz"
   wget ${DL_LINK} -P /tmp
-  tar zxvf /tmp/tfnotify_v0.6.2_linux_amd64.tar.gz -C /tmp
-  mv /tmp/tfnotify_v0.6.2_linux_amd64/tfnotify /bin/tfnotify
-  rm -rf /tmp
-  printf '\033[36m%s\033[m\n' "install tfnotify."
+  tar zxvf /tmp/tfnotify_linux_amd64.tar.gz -C /tmp
+  mv /tmp/tfnotify /bin/tfnotify
+  echo "install tfnotify"
 }
 
 install_tflint
