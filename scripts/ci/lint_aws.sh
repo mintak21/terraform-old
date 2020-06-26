a3873bb197511d0693bff65a12d86b0798440129
mintak-Macbook <mintak21@gmail.com>
2020-06-27 04:59:55 +0900

    [feat] add lint :sparkles:

--- scripts/ci/lint_aws.sh
+++ scripts/ci/lint_aws.sh
@@ -2,14 +2,14 @@
 cd `dirname $0`
 . ./settings.sh
 
-function validate() {
+function deep_lint() {
   target_dirs=`find ../../${base_dir} -type f -name "*.tf" -exec dirname {} \; | sort -u`
   for target in ${target_dirs}
   do
     cd ${target}
     terraform init -input=false -no-color
-    terraform validate -no-color
+    tflint --deep --aws-region=${aws_region}
   done
 }
 
-validate
+deep_lint
---
+++ scripts/ci/settings.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+aws_region="ap-northeast-1"
+base_dir="files"
