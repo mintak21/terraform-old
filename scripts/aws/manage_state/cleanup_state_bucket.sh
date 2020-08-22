386873672b1f6342421bda1dc521a5d375ea0be9
mintak-Macbook <mintak21@gmail.com>
2020-08-22 12:12:38 +0900

    [fix] fix shell lint check

--- scripts/aws/manage_state/cleanup_state_bucket.sh
+++ scripts/aws/manage_state/cleanup_state_bucket.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-cd `dirname $0`
+cd "$(dirname $0)" || exit 1
 . ./settings.sh
 
-
-function delete_S3_bucket() {
-  aws s3api delete-bucket --bucket ${bucket_name}
+delete_S3_bucket() {
+  aws s3api delete-bucket --bucket "${bucket_name}"
   printf '\033[91m%s\033[m\n' "deleted state bucket: ${bucket_name}."
 }
 
-function delete_dynamoDB() {
-  aws dynamodb delete-table --table-name ${state_lock_table_name}
+delete_dynamoDB() {
+  aws dynamodb delete-table --table-name "${state_lock_table_name}"
   printf '\033[91m%s\033[m\n' "deleted dynamoDB table: ${state_lock_table_name}."
 }
 
--- scripts/aws/manage_state/create_state_bucket.sh
+++ scripts/aws/manage_state/create_state_bucket.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
-cd `dirname $0`
+cd "$(dirname $0)" || exit 1
 . ./settings.sh
 
-function create_S3_bucket() {
+create_S3_bucket() {
   # バケット作成
-  aws s3api create-bucket           --bucket ${bucket_name} --create-bucket-configuration LocationConstraint=${region}
+  aws s3api create-bucket --bucket "${bucket_name}" --create-bucket-configuration LocationConstraint="${region}"
   # バージョニングの有効化
-  aws s3api put-bucket-versioning   --bucket ${bucket_name} --versioning-configuration Status=Enabled
+  aws s3api put-bucket-versioning --bucket "${bucket_name}" --versioning-configuration Status=Enabled
   # バケットの暗号化
-  aws s3api put-bucket-encryption   --bucket ${bucket_name} \
-            --server-side-encryption-configuration '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}'
+  aws s3api put-bucket-encryption --bucket "${bucket_name}" \
+    --server-side-encryption-configuration '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}'
   # ブロックパブリックアクセス
-  aws s3api put-public-access-block --bucket ${bucket_name} \
-            --public-access-block-configuration '{
+  aws s3api put-public-access-block --bucket "${bucket_name}" \
+    --public-access-block-configuration '{
               "BlockPublicAcls"      : true,
               "IgnorePublicAcls"     : true,
               "BlockPublicPolicy"    : true,
               "RestrictPublicBuckets": true
             }'
   # タグ付け
-  aws s3api put-bucket-tagging      --bucket ${bucket_name} \
-            --tagging 'TagSet=[{Key=description,Value=manage mintak-terraformer tfstate}]'
+  aws s3api put-bucket-tagging --bucket "${bucket_name}" \
+    --tagging 'TagSet=[{Key=description,Value=manage mintak-terraformer tfstate}]'
   printf '\033[36m%s\033[m\n' "created state bucket: ${bucket_name}."
 }
 
-function create_dynamoDB() {
+create_dynamoDB() {
   aws dynamodb create-table \
-      --table-name ${state_lock_table_name} \
-      --attribute-definitions '[{"AttributeName":"LockID","AttributeType": "S"}]' \
-      --key-schema '[{"AttributeName":"LockID","KeyType": "HASH"}]' \
-      --provisioned-throughput '{"ReadCapacityUnits": 1, "WriteCapacityUnits": 1}'
+    --table-name "${state_lock_table_name}" \
+    --attribute-definitions '[{"AttributeName":"LockID","AttributeType": "S"}]' \
+    --key-schema '[{"AttributeName":"LockID","KeyType": "HASH"}]' \
+    --provisioned-throughput '{"ReadCapacityUnits": 1, "WriteCapacityUnits": 1}'
   printf '\033[36m%s\033[m\n' "created dynamoDB table for lock: ${state_lock_table_name}."
 }
 
